4fa249d3497f360a48a642ca07a02dc9682e9eb6
Leonardo França <leoprofissional@live.com>
2025-07-10 19:13:00 -0300

    melhora a interface da tela de lista de compras

--- resources/views/livewire/list/index.blade.php
+++ resources/views/livewire/list/index.blade.php
@@ -1,18 +1,26 @@
 <div class="flex flex-col gap-2">
     <x-card>
         <form wire:submit="save" class="flex flex-col justify-center gap-2">
-            <div class="w-full">
-                <x-input class="w-full uppercase" wire:model="item"/>
+            <div class="flex flex-col gap-2">
+                <div class="flex-1">
+                    <x-input
+                        label="Descrição"
+                        class="uppercase"
+                        wire:model="item"
+                    />
+                </div>
+                <div class="flex-1">
+                    <x-number label="Quantidade" wire:model="quantity"/>
+                </div>
             </div>
-            <div class="w-full">
-                <x-number class="w-full" wire:model="quantity"/>
+            <div class="flex gap-2">
+                <x-button type="submit" class="flex-2">
+                    Adicionar
+                </x-button>
+                <x-button wire:click="clear" color="red" class="flex-1">
+                    <x-icon name="trash" class="h-5 w-5"/>
+                </x-button>
             </div>
-            <x-button type="submit">
-                Adicionar
-            </x-button>
-            <x-button wire:click="clear" color="red">
-                <x-icon name="trash" class="h-5 w-5"/>
-            </x-button>
         </form>
 
     </x-card>
