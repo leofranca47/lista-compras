763f3e2e0ec5950f235c33d24ae58d8502b1015e
Leonardo França <leoprofissional@live.com>
2026-01-25 16:28:36 -0300

    feat: correção na tela de login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use Illuminate\View\View;
 
 use function auth;
@@ -14,8 +15,9 @@ class AuthenticatedSessionController
 {
     public function create(): View|RedirectResponse
     {
+        Session::regenerate();
         if (auth()->check()) {
-            return redirect()->route('dashboard');
+            return redirect()->intended(route('dashboard', absolute: false));
         }
 
         return view('auth.login');
